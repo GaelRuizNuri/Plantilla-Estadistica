4166642a8c2217246652a0062754b3072fa6f3ca
GaelRuizNuri <g.ruiz.1@estudiantec.cr>
2026-05-21 17:54:51 -0600

    Implentación de ANOVA y Pruebas de Independencia. Se mejoró la Prueba de Bondad y Ajuste.

--- plantilla.py
+++ plantilla.py
@@ -3,35 +3,31 @@ import numpy as np
 
 
 def probabilidades_discretas(dist, expresiones):
-    probs = {}
+    probs = []
     for expresion in expresiones:
         
-        if isinstance(expresion, tuple) and len(expresion) == 2:
-            indice, k = expresion
-            probs[indice] = dist.pmf(k) # P(X = k)
+        if isinstance(expresion, int):
+            probs.append(dist.pmf(expresion)) # P(X = k)
 
         else:
-            indice, a, b = expresion
-
-            if a is None: probs[indice] = dist.cdf(b) # P(X <= b)
-
-            elif b is None: probs[indice] = dist.sf(a - 1) # P(X >= a)
-
-            else: probs[indice] = dist.cdf(b) - dist.cdf(a - 1) # P(a <= X <= b)
+            a, b = expresion
+            if a is None: probs.append(dist.cdf(b)) # P(X <= b)
+            elif b is None: probs.append(dist.sf(a - 1)) # P(X >= a)
+            else: probs.append(dist.cdf(b) - dist.cdf(a - 1)) # P(a <= X <= b)
 
     return probs
 
 
 def probabilidades_continuas(dist, expresiones):
-    probs = {}
+    probs = []
 
     for expresion in expresiones:
-        indice, a, b = expresion
-        if a is None: probs[indice] = dist.cdf(b) # P(X < b)
+        a, b = expresion
+        if a is None: probs.append(dist.cdf(b)) # P(X < b)
 
-        elif b is None: probs[indice] = dist.sf(a) # P(X > a) v P(a < X)
+        elif b is None: probs.append(dist.sf(a)) # P(X > a) v P(a < X)
 
-        else: probs[indice] = dist.cdf(b) - dist.cdf(a) # P(a < X < b)
+        else: probs.append(dist.cdf(b) - dist.cdf(a)) # P(a < X < b)
 
     return probs
 
@@ -66,21 +62,73 @@ def chi_obs_bondad_ajuste(observados, probs):
     observados, esperados = agrupar_esp(observados, esperados)
 
     chi_observado = sum((fo-fe)*(fo-fe)/fe for fo, fe in zip(observados, esperados))
-    print(f'Observados     | {observados}')
-    print(f'Probabilidades | {list(probs.values())}')
-    print(f'Esperados      | {esperados}')
+    gl = len(observados)
+    print(f'Observados: {observados}')
+    print(f'Probabilidades: {probs}')
+    print(f'Esperados: {esperados}')
+    print(f'Grados de libertad: {gl}')
     print(f'Chi Cuadrado observado: {chi_observado}')
-    return chi_observado
+    return chi_observado, gl
+
+
+def f_obs_anova(grupos):
+    k = len(grupos)
+    n = [len(grupo) for grupo in grupos]
+    t = [sum(grupo) for grupo in grupos]
+    N = sum(n)
+    T = sum(t)
+    datos = [x for grupo in grupos for x in grupo]
+    suma_cuadrados = sum(x*x for x in datos)
+    SST = suma_cuadrados - T*T/N
+    SSA = sum([t[i]*t[i]/n[i] for i in range(k)]) - T*T/N
+    SSE = SST - SSA
+    f_obs = (SSA/SSE)*(N-k)/(k-1)
+    gl = (k-1, N-k)
+    print(f'k = {k}\n'
+          f'n_i = {n}\n'
+          f'T_i = {t}\n'
+          f'N = {N}\n'
+          f'T = {T}\n'
+          f'ΣΣ(y_i_j)^2 = {suma_cuadrados}\n'
+          f'SST = {SST}\n'
+          f'SSA = {SSA}\n'
+          f'SSE = {SSE}\n'
+          f'Grados de libertad = {gl}\n'
+          f'F_obs = {f_obs}')
+
+    return f_obs, gl
+
+def chi_obs_independencia(tabla_obs):
+    fil = len(tabla_obs)
+    col = len(tabla_obs[0])
+
+    total_cols = [sum(tabla_obs[i][j] for i in range(fil)) for j in range(col)]
+    total_fils = [sum(fila) for fila in tabla_obs]
+    n = sum(total_fils)
+    tabla_esp = [[total_fils[i]*total_cols[j]/n for j in range(col)] for i in range(fil)]
+    chi_obs = sum(((fo-fe)**2)/fe for fila_obs, fila_esp in zip(tabla_obs, tabla_esp) for fo, fe in zip(fila_obs, fila_esp))
+    gl = (fil-1)*(col-1)
+    print(f'Total de cada fila: {total_fils}\n' 
+          f'Total de cada columna: {total_cols}\n' 
+          f'Total de fila × columna: {n}'
+    )
+    print("Esperados")
+    for fila in tabla_esp: print(fila)
+
+    print(f'Chi Cuadrado observado: {chi_obs}\n'
+          f'Grados de libertad = {gl}'
+    )
+    return chi_obs,gl
 
 #Ej práctica #3 de la PPT 
 expresiones = [
-    (0,None,1.45),  # x < 1.45
-    (1,1.45,1.55),  # 1.45 < x < 1.55
-    (2,1.55,1.65),  # 1.55 < x < 1.65
-    (3,1.65,1.75),  # 1.65 < x < 1.75
-    (4,1.75, 1.85), # 1.75 < x < 1.85
-    (5,1.85, 1.95), # 1.85 < x < 1.95
-    (6,1.95, None)  # 1.95 < x
+    (None,1.45),  # x < 1.45
+    (1.45,1.55),  # 1.45 < x < 1.55
+    (1.55,1.65),  # 1.55 < x < 1.65
+    (1.65,1.75),  # 1.65 < x < 1.75
+    (1.75, 1.85), # 1.75 < x < 1.85
+    (1.85, 1.95), # 1.85 < x < 1.95
+    (1.95, None)  # 1.95 < x
 ]
 
 dist = norm(loc=1.76, scale=0.1)
